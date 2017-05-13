33fea6c1756ae0d5583064f56d02eec64d87cdfa
NikolaeVarius <joekim29@gmail.com>
2017-05-13 18:23:59 -0400

    random

--- parro_xy/index.js
+++ parro_xy/index.js
@@ -8,23 +8,42 @@ var client  = arDrone.createClient();
 client.createRepl();
 
 
-app.get('/api/users', function(req, res) {
-  var user_id = req.param('id');
-  var token = req.param('token');
-  var geo = req.param('geo');
+// app.get('/api/users', function(req, res) {
+//   var user_id = req.param('id');
+//   var token = req.param('token');
+//   var geo = req.param('geo');
 
-  res.send(user_id + ' ' + token + ' ' + geo);
+//   res.send(user_id + ' ' + token + ' ' + geo);
+// });
+
+// Random Fun Shit
+
+
+
+
+//
+app.post('/drone/flightdemp', function(req, res) {
+    console.log('Testing Flight Demo');
+    client.takeoff();
+
+    client
+      .after(5000, function() {
+        this.clockwise(0.5);
+      })
+      .after(3000, function() {
+        this.stop();
+        this.land();
+      });
 });
 
 
-// POST http://localhost:8080/api/users
-// parameters sent with
 app.post('/drone/blink', function(req, res) {
   client.animateLeds('blinkRed', 5, 2)
   console.log('in here');
 });
 
 
+// TODO. Handle Raw TCP/UDP Packets
 
 app.listen(port);
 console.log('Server started! At http://localhost:' + port);
